d15f35d665fdc3d57dedd81674293f6f169e3ec3
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-03-27 11:33:55 +0200

    first commit

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,32 @@
+
+
+//versione abbreviata con return implicito
+const MyComponent = () => (
+    <>
+    <h2>Lista</h2>
+    <ul>
+        <li>testo1</li>
+        <li>testo2</li>
+        <li>testo3</li>
+    </ul>
+    </>
+)
+;
+
+export default MyComponent;
+
+// const MyComponent = () => {
+
+// return (
+//     <>
+//     <h2>Lista</h2>
+//     <ul>
+//         <li>testo1</li>
+//         <li>testo2</li>
+//         <li>testo3</li>
+//     </ul>
+//     </>
+// )
+// };
+
+// export default MyComponent;
